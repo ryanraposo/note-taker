ade1172bb8ff6579b8de6cd496a0a13a1f1fe98b
Ryan Raposo <raposo.ryan@gmail.com>
2022-07-20 18:20:40 -0400

    add html routes

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
