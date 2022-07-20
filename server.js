82ed7ebb87ab6d9bc53695530f0c7c8c9322fcfe
Ryan Raposo <raposo.ryan@gmail.com>
2022-07-20 18:29:46 -0400

    add html routes

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
 const express = require('express');
 
 
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
 const app = express();
 
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/', htmlRoutes);
 
 
 const PORT = process.env.PORT || 3001;
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
 
 
 app.listen(PORT, () => {
