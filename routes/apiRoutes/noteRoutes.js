6f73c33bc7e0e4229506088b4929cd9771ceb2e3
Ryan Raposo <raposo.ryan@gmail.com>
2022-07-20 18:51:06 -0400

    add api routes

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const router = require('express').Router();
+const { uid } = require('uid');
+const { validateNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    if (notes) {
+        res.json(results);
+    } else {
+        res.sendStatus(404);
+    };
+});
+
+
+router.post('/notes', (req, res) => {
+    req.body.id = uid();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send("Improperly formatted note.")
+    } else {
+        
+    }
+    
+});
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 
 const app = express();
